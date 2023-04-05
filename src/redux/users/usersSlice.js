b015b1278cc1ccd6eb5974c18f62881f9bffe4a6
isaacg <iwanyoike65@gmail.com>
2023-04-05 17:18:30 +0300

    second with builders and thunkapi

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -8,11 +8,15 @@ const initialState = {
 
 export const fetchUsers = createAsyncThunk(
     "users/fetchUsers",
-    async () => {
-        const response = await fetch("https://randomuser.me/api/?results=5");
-        const data = await response.json();
-        console.log(data)
-        return data.results;
+    async (_, thunkAPI) => {
+        try{
+            const response = await fetch("https://randomuser.me/api/?results=5");
+            const data = await response.json();
+            return data.results;
+        }
+        catch(error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
     }
 );
 
@@ -21,18 +25,19 @@ export const usersSlice = createSlice({
     name: "users",
     initialState,
     reducers: {},
-    extraReducers: {
-        [fetchUsers.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [fetchUsers.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.users = action.payload;
-        },
-        [fetchUsers.rejected]: (state, action) => {
-            state.isLoading = false;
-            state.error = action.error.message;
-        }
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.users = action.payload;
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
     },
 
 
